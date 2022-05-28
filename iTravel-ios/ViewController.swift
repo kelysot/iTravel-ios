aee0030580bd9407754584756185cd07ba6af8f3
barelimelech <barelimelech01@gmail.com>
2022-05-28 16:01:03 +0300

    Initial Commit

---
+++ iTravel-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTravel-ios
+//
+//  Created by Bar Elimelech on 28/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
