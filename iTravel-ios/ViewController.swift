dcb00664e7a502ff2583b03ac392e684b7414dc4
kelysot <kelysot@gmail.com>
2022-05-31 17:44:59 +0300

    Check.

--- iTravel-ios/ViewController.swift
+++ iTravel-ios/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Check if working.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
