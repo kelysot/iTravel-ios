8eb45c3fbfd6e2367e96c5f76ae7a89237516ffa
kelysot <kelysot@gmail.com>
2022-07-19 11:37:46 +0300

    delete print

--- iTravel-ios/ModelFirebase.swift
+++ iTravel-ios/ModelFirebase.swift
@@ -159,7 +159,6 @@ class ModelFirebase{
     func createUser(email: String, password: String, completionBlock: @escaping (_ success: Bool) -> Void) {
         Auth.auth().createUser(withEmail: email, password: password) {(authResult, error) in
             if let user = authResult?.user {
-                print(user)
                 completionBlock(true)
             } else {
                 print("TAG USER \(error)")
